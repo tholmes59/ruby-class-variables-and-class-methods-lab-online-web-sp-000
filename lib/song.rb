f1e3095a0a972af39b086ea5e7194d52369362d3
Tom Holmes <tholmes59@gmail.com>
2019-01-23 23:58:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,22 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0 
+  @@genre = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count +=1
+    @@genre << self
   end
   
   def self.count
     @@count
+  end 
+  
+  def self.genre
+    @@genre = []
   end 
   
 end
