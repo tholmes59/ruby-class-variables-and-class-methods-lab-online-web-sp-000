3776e7715712879d49f1c2f9451368034b9ac43e
Tom Holmes <tholmes59@gmail.com>
2019-01-24 00:59:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genres|
-    genre_hash[genres] = name 
+    genre_hash[genres] = @@count 
     end
     genre_hash
   end 
